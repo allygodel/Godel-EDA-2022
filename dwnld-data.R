9465873fbbbd6ed834c0c14bc3959a16e9243594
Allyson Godel <dv4218bx@mnstate.edu>
2022-05-03 11:10:05 -0500

    Knit Commit

--- dwnld-data.R
+++ dwnld-data.R
@@ -4,7 +4,7 @@ download.file(KidsCount1_URL, "data/KidsCountInfluenza.xlsx", mode = "wb")
 KidsCount2_URL <- "https://datacenter.kidscount.org/rawdata.axd?ind=10883&loc=1"
 download.file(KidsCount2_URL, "data/KidsCountSARS.xlsx", mode = "wb")
 
-CDCSARS <- "https://covidtracking.com/data/download/national-history.csv"
+SARSTracker<- "https://covidtracking.com/data/download/national-history.csv"
 download.file(CDCSARS, "data/CDCSARSTracker.csv")
 
 FluView_StackedColumnChart_Data <-
